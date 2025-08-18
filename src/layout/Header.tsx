083a2a3a306494b3c3c324dfff4fcfaf2343574a
Arifur Rahaman <arifurrahaman561@gmail.com>
2025-08-18 18:57:24 +0600

    wip: responsive view

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,14 +10,14 @@ const Header = () => {
 	];
 	return (
 		<header>
-			<div className='container py-9 flex items-center'>
+			<div className='container py-9 hidden lg:flex'>
 				<div className='text-2xl font-bold'>
 					<Link href='/'>
 						<Image src='/fametonic-logo.svg' alt='Fametonic Logo' width={173} height={74} />
 					</Link>
 				</div>
 				<nav className='ml-auto'>
-					<ul className='flex space-x-10'>
+					<ul className='flex gap-11'>
 						{menuItems.map((item) => (
 							<li key={item.label}>
 								<a href={item.href} className='text-[#A9A9A9] text-[18px] font-semibold font-figtree'>
@@ -25,6 +27,31 @@ const Header = () => {
 						))}
 					</ul>
 				</nav>
+			</div>
+			<div className='flex justify-end lg:hidden text-white container py-9 gap-[93px]'>
+				<div className='text-2xl font-bold'>
+					<Link href='/'>
+						<Image src='/fametonic-logo.svg' alt='Fametonic Logo' width={173} height={74} />
+					</Link>
+				</div>
+				<Drawer direction='right'>
+					<DrawerTrigger className='relative'>
+						<Image src='/hamburger-icon.svg' alt='Menu' width={24} height={24} className='cursor-pointer' />
+					</DrawerTrigger>
+					<DrawerContent className='w-[310px] flex bg-white'>
+						<nav className='p-8'>
+							<ul className='flex flex-col gap-4'>
+								{menuItems.map((item) => (
+									<li key={item.label}>
+										<a href={item.href} className='text-[#A9A9A9] text-[18px] font-semibold font-figtree'>
+											{item.label}
+										</a>
+									</li>
+								))}
+							</ul>
+						</nav>
+					</DrawerContent>
+				</Drawer>
 			</div>
 		</header>
 	);
--- src/layout/TopBanner.tsx
+++ src/layout/TopBanner.tsx
@@ -1,8 +1,8 @@
 const TopBanner = () => {
 	return (
-		<div className='min-h-11.5 py-2.5 text-5.5 font-extrabold text-white bg-top-banner font-figtree'>
-			<div className='container flex items-center justify-center'>
-				🚀 <span className='text-[#00E7F9] mr-1'>FRESH BEGINNINGS SALE: </span> Extra 25% OFF, Limited Spots - start your journey today!
+		<div className='min-h-11.5 py-5 lg:py-2.5 font-extrabold text-white bg-top-banner font-figtree'>
+			<div className='container block lg:flex items-center justify-center leading-none lg:leading-[normal] text-[14px] lg:text-5.5 text-center'>
+				🚀 <span className='text-[#00E7F9] text-base lg:text-5.5 mr-1'>FRESH BEGINNINGS SALE: </span> Extra 25% OFF, Limited Spots - start your journey today!
 			</div>
 		</div>
 	);
